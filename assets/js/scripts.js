1ec9d66bef341dcf7786aa1b3a39dec039cf4c2d
John-Yip-HK <yip4work@gmail.com>
2022-02-09 14:17:15 +0800

    Finish tooltip

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,7 @@
 import * as d3 from "https://cdn.skypack.dev/d3@7";
 
-const WIDTH = 800;
-const HEIGHT = 400;
+const WIDTH = 700;
+const HEIGHT = 500;
 const PADDING = 50;
 
 const container = d3.select("div#container");
@@ -17,14 +17,17 @@ fetch(
   .then((res) => res.json())
   .then((data) => {
     const parseMinutes = (minute) => {
+      const date = new Date();
       const minSec = minute.split(":");
-      return +minSec[0] * 60 + +minSec[1];
+
+      date.setMinutes(+minSec[0], +minSec[1]);
+      return date;
     };
 
     const parseYear = (year) => new Date(Date.parse(year));
 
     const minuteScale = d3
-      .scaleLinear()
+      .scaleTime()
       .domain([
         d3.min(data, (record) => parseMinutes(record["Time"])),
         d3.max(data, (record) => parseMinutes(record["Time"])),
@@ -34,10 +37,51 @@ fetch(
     const yearScale = d3
       .scaleTime()
       .domain([
-        d3.min(data, (record) => parseYear(record["Year"])),
-        d3.max(data, (record) => parseYear(record["Year"])),
+        parseYear(d3.min(data, (record) => record["Year"]) - 1),
+        parseYear(d3.max(data, (record) => record["Year"]) + 1),
       ])
       .range([PADDING, WIDTH - PADDING]);
+
+    const xAxis = d3.axisBottom(yearScale);
+    const yAxis = d3.axisLeft(minuteScale).tickFormat(d3.timeFormat("%M:%S"));
+
+    const tooltip = container.append("div").attr("id", "tooltip");
+
+    function pointerOver(event) {
+      const pointer = d3.pointer(event, d3.select("body").node());
+      const circle = d3.select(this);
+
+      const minSecTime = (dateStr) => {
+        let splitDateStr = dateStr.split(" ")[4];
+        splitDateStr = splitDateStr.split(":");
+        let minute = splitDateStr[1];
+        let second = splitDateStr[2];
+
+        return minute + ":" + second;
+      };
+
+      let html = `${circle.attr("data-name")}: ${circle.attr(
+        "data-nationality"
+      )}<br>
+      Year: ${circle.attr("data-xvalue")}, Time: ${minSecTime(
+        circle.attr("data-yvalue")
+      )}`;
+
+      if (circle.attr("data-doping").length > 0) {
+        html += `<br><br>${circle.attr("data-doping")}`;
+      }
+
+      tooltip
+        .style("display", "block")
+        .style("top", `${pointer[1]}px`)
+        .style("left", `${pointer[0] + 20}px`)
+        .attr("data-year", circle.attr("data-xvalue"))
+        .html(html);
+    }
+
+    function pointerOut() {
+      tooltip.style("display", "none").text("");
+    }
 
     svg
       .selectAll("circle")
@@ -45,16 +89,39 @@ fetch(
       .enter()
       .append("circle")
       .attr("class", "dot")
-      .attr("data-xvalue", (d) => d["Years"])
-      .attr("data-yvalue", (d) => {
-        const date = new Date(Date.parse(`${d["Year"]}-01-01`));
-        const minSec = d["Time"].split(":");
-
-        date.setMinutes(minSec[0], minSec[1]);
-        return date;
-      })
+      .attr("data-xvalue", (d) => d["Year"])
+      .attr("data-yvalue", (d) => parseMinutes(d["Time"]))
+      .attr("data-name", (d) => d["Name"])
+      .attr("data-nationality", (d) => d["Nationality"])
+      .attr("data-doping", (d) => d["Doping"])
       .attr("r", 5)
       .attr("cx", (d) => yearScale(parseYear(d["Year"])))
-      .attr("cy", (d) => minuteScale(parseMinutes(d["Time"])));
+      .attr("cy", (d) => minuteScale(parseMinutes(d["Time"])))
+      .style("fill", (d) =>
+        d["Doping"].length > 0 ? "rgb(31, 119, 180)" : "orange"
+      )
+      .style("fill-opacity", "0.8")
+      .on("mouseover", pointerOver)
+      .on("mouseout", pointerOut);
+
+    svg
+      .append("g")
+      .attr("id", "x-axis")
+      .attr("transform", `translate(0, ${HEIGHT - PADDING})`)
+      .call(xAxis);
+
+    svg
+      .append("g")
+      .attr("id", "y-axis")
+      .attr("transform", `translate(${PADDING}, 0)`)
+      .call(yAxis);
+
+    svg
+      .append("text")
+      .style("font-size", "0.5rem")
+      .attr("transform", "rotate(-90)")
+      .attr("x", -WIDTH / 4)
+      .attr("y", 10)
+      .text("Time in Minutes");
   })
   .catch((err) => alert(err.message));
